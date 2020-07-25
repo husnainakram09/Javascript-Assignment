0cc3a11d58e2d5bd3a42906d48fc8335b61851e8
husnainakram09 <husnainakram7860@gmail.com>
2020-07-25 22:33:39 +0500

    Javascript-Assignment

---
+++ app.js
@@ -0,0 +1,472 @@
+//ALERTS
+
+
+//ASSIGNMENT NO. 1
+// alert("Error! Please enter a valid password.")
+
+//ASSIGNMENT NO. 2
+// alert("Welcome to JS land.../rHappy Coding!")
+
+
+
+// VARIABLES FOR STRINGS
+
+
+// ASSIGNMNET NO.1
+// var username;
+// var myName ="Husnain Akram"
+// alert("Hello World"+ " " + myName)
+
+
+// ASSIGNMENT NO. 2
+// var studentName = "Ali"
+// var age = "20 years old"
+// var certification = "Certified Mobile Application Development"
+// alert(studentName)
+// alert(age)
+// alert(certification)
+
+
+
+// ASSIGNMENT NO. 3
+// var email ="Husnainakram7860@gmail.com"
+// alert('My email is'+ ' ' + email)
+
+
+
+// ASSIGNMENT NO. 4
+// var book = "A smarter way to learn JavaScript"
+// alert("I am trying to learn from book"+ " " + book)
+
+
+// ASSIGNMENT NO. 5
+// document.write("Yah! I can write HTML content through JavaScript")
+
+
+// ASSIGNMENT NO. 5
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(design)
+
+
+
+// VARIABLES FOR NUMBERS
+
+
+// ASSIGNMENT NO. 1
+// var age = 15
+// alert("I am " + age + " year old")
+
+
+// ASSIGNMENT NO. 2
+// var birthDayYear = 1999
+// document.write("My birthday year is " + birthDayYear)
+
+
+
+// ASSIGNMENT NO. 3
+// var visitorName = "John Doe"
+// var productTitle = "T-Shirts"
+// var quantity = 5
+// document.write(visitorName + " ordered " + quantity + " " + productTitle + " on XYZ Clothing store")
+
+
+
+// VARIABLE LEGALL & ILLEGAL
+
+// ASSIGNMENT NO. 1
+// var a = "variable 1"
+//     b = "variable 2"
+//     c = "variable 3"
+//     console.log()
+
+
+// ASSIGNMENT NO. 2
+// illegal variables
+// var %a = "variable no.1"
+// var 2b = "variable no.2"
+// var /c = "variable no.3"
+// var -d = "variable no.4"
+// var %a = "variable no.5"
+
+// // legal variables
+// var a = "variable no.1"
+// var _b = "variable no.2"
+// var $c ="variable no.3"
+// var myName = "variable no.4"
+
+
+
+
+
+// ASSIGNMENT NO. 3
+// var a = "<h1>Rules for naming JS variables</h1>"
+// var b = "Variable names can only contain numbers, $, and _.For example $my_1stVariable <br />"
+// var c = "Variables must begin with a letter, $ or _. For example $name, _name or name <br />"
+// var d = "Variable names are case sensitive <br />"
+// var e = "Variable names should not be JS keywords <br />"
+// document.write(a, b, c, d, e)
+
+
+
+// MATH EXPRESSIONS 
+
+
+// ASSIGNMENT NO.1
+// var a = 3
+// var b = 5
+// var c = a + b
+// document.write("Sum of " + a + " and " + b + " is " + c)
+
+
+// ASSIGNMENT NO.2
+// var a = 6
+// var b = 3
+// var c = a / b
+// document.write("Division of " + a + " and " + b + " is " + c)
+
+
+// ASSIGNMENT NO.3
+// var a = 10
+// var b = 3
+// var c = a % b
+// document.write("Modulus of " + a + " and " + b + " is " + c)
+
+// ASSIGNMENT NO.4
+// var a = 5
+// var b = 3
+// var c = a - b
+// document.write("Subtraction of " + a + " and " + b + " is " + c)
+
+
+// ASSIGNMENT No.5
+// var a;
+// document.write("Value after variable declaration is " + a + "<br />")
+// var a = 5
+// document.write("Initial value is " + a + "<br />")
+// a++;
+// document.write("After increment value is " + a + "<br />")
+// var b = a + 7
+// document.write("After addition " + b + "<br />")
+// b--;
+// document.write("Value after decrement is: " + b + "<br />")
+
+// document.write("Value after decrement is: " + b + "<br />")
+
+
+
+// Chapter 4
+
+
+// Assignment no. 1 
+// document.write("<h1>Result:</h1>")
+// var a = 10
+// document.write("The value of a is: " + a + "<br />")
+// var a = ++a;
+// document.write("The value of ++a is : " + a + "<br />")
+// var a;
+// document.write("Now the value of a is: " + a + "<br />")
+// var a = a++
+// document.write("The value of a++ is :" + a + "<br />")
+// a++;
+// document.write("Now the value of a is :" + a + "<br />")
+// var a = --a
+// document.write("The value of --a is :" + a + "<br />")
+// var a; 
+// document.write("Now the value of a is :" + a + "<br />")
+// var a = a--
+// document.write("The value of a-- is :" + a + "<br />")
+// a--;
+// document.write("Now the value of a is :" + a + "<br />")
+
+
+
+// Assignment No. 2
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+// var stageA = --a;
+// var stageB = --a - --b;
+// var stageC = --a - --b + ++b;
+// var stageD = --a - --b + ++b + b--;
+// document.write("a is :" + stageA + "<br />")
+// document.write("b is :" + stageB + "<br />")
+// document.write("c is :" + stageC + "<br />")
+// document.write("d is :" + stageD + "<br />")
+// document.write("Result is: " + result)
+
+
+// Assignment No.3
+// var userName = prompt("Enter your name")
+// document.write("Hello " + userName)
+
+
+// Assignnment no. 5
+
+// var number = +prompt("Enter Any Number")
+// if (number == ""){
+//     document.write(5 + "x" + "1" + "=" + 5 * 1 + "<br />")
+//     document.write(5 + "x" + "2" + "=" + 5 * 2 + "<br />")
+//     document.write(5 + "x" + "3" + "=" + 5 * 3 + "<br />")
+//     document.write(5 + "x" + "4" + "=" + 5 * 4 + "<br />")
+//     document.write(5 + "x" + "5" + "=" + 5 * 5 + "<br />")
+//     document.write(5 + "x" + "6" + "=" + 5 * 6 + "<br />")
+//     document.write(5 + "x" + "7" + "=" + 5 * 7 + "<br />")
+//     document.write(5 + "x" + "8" + "=" + 5 * 8 + "<br />")    
+//     document.write(5 + "x" + "9" + "=" + 5 * 9 + "<br />")
+//     document.write(5 + "x" + "10" + "=" + 5 * 10 + "<br />")
+// } else {
+//     document.write(number + "x" + "1" + "=" + number * 1 + "<br />")
+//     document.write(number + "x" + "2" + "=" + number * 2 + "<br />")
+//     document.write(number + "x" + "3" + "=" + number * 3 + "<br />")
+//     document.write(number + "x" + "4" + "=" + number * 4 + "<br />")
+//     document.write(number + "x" + "5" + "=" + number * 5 + "<br />")
+//     document.write(number + "x" + "6" + "=" + number * 6 + "<br />")
+//     document.write(number + "x" + "7" + "=" + number * 7 + "<br />")
+//     document.write(number + "x" + "8" + "=" + number * 8 + "<br />")
+//     document.write(number + "x" + "9" + "=" + number * 9 + "<br />")
+//     document.write(number + "x" + "10" + "=" + number * 10 + "<br />")
+// }
+
+
+// Assignnment no. 6
+// var subFirst = prompt("Enter your first subject")
+// var subSecond = prompt("Enter your second subject")
+// var subThird = prompt("Enter your third subject")
+// var totalMarks = 100
+// var subFirstMarks = prompt("Enter your obtain marks of " + subFirst)
+// var subSecondMarks = prompt("Enter your obtain marks " + subSecond)
+// var subThirdMarks = prompt("Enter your obtain marks " + subThird)
+// document.write("Subjects " + "Total Marks "+ "Obtain Marks " + "<br />" + subFirst + totalMarks + subFirstMarks + "<br />" + subFirst + totalMarks + subFirstMarks)
+// // document.write(+ "<br />" + totalMarks + "<br />" + totalMarks + "<br />" + totalMarks)
+
+
+
+// IF ELSE STATEMENTS 
+
+
+
+// ASSIGNMENT NO.1
+// var cityName = prompt("Enter your city name")
+// if (cityName == "karachi"){
+//     document.write("Welcome to city of lights")
+// }
+
+
+// ASSIGNMENT NO.2
+// var gender = prompt("Enter your gender")
+// if (gender == "male"){
+//     alert("Good Morning Sir!")
+// } if (gender == "female"){
+//     alert("Good Morning Ma'am!")
+// }
+
+
+
+// ARRAYS
+
+
+
+// ASSIGNMENT NO. 1
+
+// var studentNames = []
+// var animals = ["cat", "dog", "rat"]
+// var numbers = [1, 2, 3, 4, 5]
+// var mixedArray = [5, "cat", 6.7]
+// var qualification = ["SSC", "HSSC", "BCS", "BS", "BCOM", "MS", "M.PHIL", "Ph.D"]
+// for (var i = 0 ; i <= qualification.length; ++i) {
+//     document.write(qualification[i] + "<br />")
+// }
+
+
+
+// ARRAY AND LOOPS
+
+// Assignment no. 1
+// var cat = ["cats", "dogs"]
+// var arr = [[], cat]
+// console.log()
+
+
+// Assignment no.3
+// for (var i = 1; i<=10; ++i){
+//     document.write(i + "<br />")
+// }
+
+// Assignment no. 4
+// var getValue = +prompt("Enter a number to show its multiplication table")
+// var getLength = +prompt("Enter multiplication table length")
+// for (var i = 1; i <= getLength; i++){
+//     document.write(getValue + "x" + i + "=" + getValue*i + "<br />")
+// }
+
+
+
+// Assignment no.5
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"]
+// for (var i = 0; i <= fruits.length; i++){
+//     document.write(fruits[i] + "<br />")
+// }
+
+// Assignment no. 6
+// document.write("<h1>Counting</h1>:" + "<br />" )
+// for (var i = 1; i <= 15; i++){
+//     document.write(i + ",")
+// }
+// document.write("<br />" + "<h1>Reverse counting:</h1>" + "<br />")
+// for (var i = 10; i >= 1; i--) {
+//     document.write(i + ",")
+// }
+// document.write("<br />" + "<h1>Even:</h1>" + "<br />")
+// for (var i = 0; i <= 20; i = i+2) {
+//     document.write(i + ",")
+// }
+// document.write("<br />" + "<h1>Odd:</h1>" + "<br />")
+// for (var i = 1; i <= 20; i = i+2) {
+//     document.write(i + ",")
+// }
+// document.write("<br />" + "<h1>Series:</h1>" + "<br />")
+// for (var i = 2; i <= 20; i = i+2) {
+//     document.write(i + "k" + ",")
+// }
+
+
+// Assignmnet No.7
+
+
+// var search = prompt("Welcome to ABC Bakery. What do you want to order Sir/Ma'am?")
+// var a = ["cake", "apple pie", "cookie", "chips", "patties"]
+// for (var i = 0; i <= a.length; i++){
+//     if (a[i] === search){
+//         // alert("found")
+//         alert(search + " is available at index " + a.indexOf(search) + " in our Bakery")
+//     } else {
+//         alert("We are sorry. " + search + "is not Available in our Bakery")
+//     }
+//     break;
+// }
+
+ 
+
+// Assignment no.8
+
+
+
+// var arr = [3, 6, 2, 56, 32, 5, 89, 32];
+// var largest = arr[0];
+
+// for (var i = 0; i < arr.length; i++) {
+//     if (largest < arr[i] ) {
+//         largest = arr[i];
+//     }
+// }
+// document.write("The largest number is "+ largest);
+
+
+
+// Assignment no.9
+
+
+// var a = [24, 53, 78, 91, 12]
+// var smallest = a[0];
+// for (var i = 0; i < a.length; i++){
+//     if (smallest > a[i]) {
+//         smallest = a[i]
+//     }
+// }
+// document.write("The smallest number is " + smallest)
+
+
+// Assignment no.10
+
+// for (var i = 5; i <= 100; i= i + 5){
+//     document.write(i + ",")
+// }
+
+// STRING METHODS
+
+
+// Assignment no.1 
+
+// var firstName = prompt("Enter you first name")
+// var lastName = prompt("Enter you last name")
+// document.write("Hello " + firstName + " " + lastName)
+
+
+// Assigment no. 2
+
+// var phoneName = prompt("Enter your favourite phone name")
+// document.write("My favourite phone is " + phoneName + "<br />")
+// document.write("length of string: " + phoneName.length)
+
+
+// Assignmnet no. 3
+
+// var a = "Pakistani"
+// var b = a.indexOf("n")
+// document.write("String: " + a + "<br />" + "Index of 'n': " + b)
+
+// Assignment no. 4
+
+// var a = "Hello World"
+// var b = a.lastIndexOf("l")
+// document.write("String: " + a + "<br />" + "Last index of 'l': " + b)
+
+
+// Assignment no 5
+
+// var a = "Pakistani"
+// var b = a.charAt(3)
+// document.write("string: " + a + "<br />" + " Char at index 3: " + b)
+
+
+// MATH METHODS
+
+
+// Assignment no.1
+
+// var getNumber = +prompt("Enter any positive integers")
+// var roundOff = Math.round(getNumber)
+// var floorValue = Math.floor(getNumber)
+// var ceilValue = Math.ceil(getNumber)
+// document.write("Number: " + getNumber + "<br />" + "Round of value: " + roundOff + "<br />" + "floor value: " + floorValue + "<br />" + "ceil value: " + ceilValue)
+
+
+// Assignment No. 2
+
+
+// var getFloatNumber = -prompt("Enter any negative integers")
+// var roundOff = Math.round(getNumber)
+// var floorValue = Math.floor(getNumber)
+// var ceilValue = Math.ceil(getNumber)
+// document.write("Number: " + getNumber + "<br />" + "Round of value: " + roundOff + "<br />" + "floor value: " + floorValue  + "<br />" + "ceil value: " + ceilValue)
+
+
+
+// Date
+
+
+
+// Assignment No. 1
+
+// var todayDate = new Date()
+// document.write(todayDate)
+
+
+// Assignment No. 1
+
+// var monthNames = ["January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+// var now = new Date();
+// var theMonth = now.getMonth();
+// var nameOfMonth = monthNames[theMonth];
+// document.write(nameOfMonth)
+
+// Assignment no. 2
+
+// var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// var now = new Date();
+// var theDay = now.getDay();
+// var nameOfToday = dayNames[theDay];
+// document.write("Today is " + nameOfToday)
+
+// Assignement no.3
+
+
